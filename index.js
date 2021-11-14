50c73d84382d763593808464c9b998a469d1f15c
Subi <asubigyan@gmail.com>
2021-11-14 14:30:52 +0545

    correct environment variable

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ connectDB();
 
 app.use("/api/transactions", transactionRouter);
 
-if (process.env.NODE_ENV == "production") {
+if (environment == "production") {
     app.use(express.static("client/build"));
     app.get("*", (req, res) =>
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
